644a07ffcf54c4b831d30055812b8cecab2ad1e1
LiiChar <yura22875@yandex.ru>
2024-11-26 00:36:37 +0500

    v22112024-2

--- itihas_api/dist/src/lib/num.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.randomRangeInt = void 0;
-const randomRangeInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-exports.randomRangeInt = randomRangeInt;
--- itihas_api/dist/src/test/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const actions_1 = require("./actions");
-(0, actions_1.actionsTest)();
